eb1280d192c27c446660b5fc25eafb5ca99fea07
Bilal AHmad <bilal2q2@gmail.com>
2020-12-27 14:17:25 +0500

    with stages

--- hello.py
+++ hello.py
@@ -1,4 +1,61 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 words_list =['ardvark','baboon','camel']
 word = random.choice(words_list)
 word_length = len(word)
@@ -8,10 +65,13 @@ display = []
 for j in range(word_length):
     display.append('-')
 
-while '-' in display:
+game_over = False
+while not game_over:
     guess = input("Enter a letter\n").lower()
     for i in range(word_length):
         if guess == word[i]:
             display[i] = word[i]
     print(display)
-print('You win')
+    if '-' not in display:
+        game_over = True
+        print('You Win ')
