1d475fb3964ea84484a0e0e9aabdf1ad5137ca7b
Bilal AHmad <bilal2q2@gmail.com>
2020-12-27 14:36:51 +0500

    testing for stages

--- pw.py
+++ pw.py
@@ -1,4 +1,57 @@
-#TODO-1: - Use a while loop to let the user guess again. 
-# The loop should only stop once the user has guessed all 
-# the letters in the chosen_word and 'display' has no more 
-# blanks ("_"). Then you can tell the user they've won.
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+print(stages[7])
