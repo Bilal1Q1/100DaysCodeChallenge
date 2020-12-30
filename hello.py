f86179d6ccc7c0831404957ef2cdc4bdeff985f2
Bilal Ahmad <bilal2q2@gmail.com>
2020-12-30 14:17:34 +0500

    replit

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
-#Hello Dictionaries
+from art import logo
+
+print('Welcome to "Blink Auction Program"')
+
+
+def highest_bid(bidding_record):
+    highest = 0
+    bidder = ''
+    # print(bidding_record)
+    for key in bidding_record:
+        if highest < bidding_record[key]:
+            highest = bidding_record[key]
+            bidder = key
+    print(f'{bidder} won the bid with {highest}$')
+print(logo)
+bidder = {}
+bidding_continue = False
+while not bidding_continue:
+    name = input('What is your name ?\n')
+    bid = int(input('Enter Your Bid Amount. $'))
+    bidder[name] = bid
+    bid_contnue = input('Are there any other bidders ? type\'yes\' or \'no\' : ').lower()
+    if bid_contnue == 'no':
+        bidding_continue = True
+        highest_bid(bidder)
