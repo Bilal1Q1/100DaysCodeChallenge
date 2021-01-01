9d17bb43b2067c765d33032783a1bd7f407bf573
Bilal Ahmad <bilal2q2@gmail.com>
2021-01-01 13:22:50 +0500

    Day_10_Calc

--- hello.py
+++ hello.py
@@ -10,7 +10,16 @@ def Calculator(f_num, operator, s_num):
     else:
         return 'Wrong Operator'
 first_num = int(input('Enter first Number: '))
-operator = input('\n+\n-\n*\n\\\n\nChoose operation: ')
-second_number = int(input('Enter Second Numnber: '))
-result = Calculator(first_num, operator, second_number)
-print(result)
+game_over = False
+while not game_over:
+
+    operator = input('\n+\n-\n*\n\\\n\nChoose operation: ')
+    second_number = int(input('Enter Second Numnber: '))
+    result = Calculator(first_num, operator, second_number)
+    print(f"{first_num} {operator} {second_number} = {result}")
+    continu = input(f'Type \'y\' to continue calculating with {result}, or type \'n\' to start a new calculatio: ').lower()
+    if continu == 'no':
+        game_over = True
+    else:
+        first_num = result
+        
