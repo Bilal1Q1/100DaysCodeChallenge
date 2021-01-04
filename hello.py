536699cdfc9687407d33992fcdef39a495c978e3
Bilal Ahmad <bilal2q2@gmail.com>
2021-01-04 13:16:26 +0500

    From Office

--- hello.py
+++ hello.py
@@ -4,7 +4,8 @@ import random
 
 
 
-# Hint 4
+
+# Hint 4:
 def deal_card():
     cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
     card = random.choice(cards)
@@ -22,7 +23,6 @@ def calculate_score(cards):
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
-    return sum(cards)
     return sum(cards)
 print(calculate_score(user_cards))
 print(calculate_score(computer_cards))
