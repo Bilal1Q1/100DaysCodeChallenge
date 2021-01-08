c7ab68556056f25dd05c67061f05d0b21d057433
Bilal Ahmad <bilal2q2@gmail.com>
2021-01-08 17:54:19 +0500

    Day_11_last

--- hello.py
+++ hello.py
@@ -9,9 +9,21 @@ def deal_card():
     return card
 user_cards = []
 machine_cards = []
+game_over = False
 for _ in range(2):
     user_cards.append(deal_card())
     machine_cards.append(deal_card())
+print(f'\tyour cards: {user_cards}, current score: {sum(user_cards)}')
+print(f'\tcomputer\'s first card: {machine_cards[0]}')
+while not game_over:
+    continue_or_pass = input('Type \'y\' to get another card, type \'n\' to pass:\t').lower()
+    if continue_or_pass == 'y':
+        user_cards.append(deal_card())
+        machine_cards.append(deal_card())
+        print(f'\tyour cards: {user_cards}, current score: {sum(user_cards)}')
+        print(f'\tcomputer\'s first card: {machine_cards[0]}')
+    else:
+        game_over = True
 
 print(user_cards)
 print(machine_cards)
