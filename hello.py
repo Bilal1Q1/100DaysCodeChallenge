14f923eee434543c0ba243f1975932fc060e6514
Bilal Ahmad <bilal2q2@gmail.com>
2021-01-09 10:57:28 +0500

    final_Day_11

--- hello.py
+++ hello.py
@@ -1,29 +1,60 @@
 import random
-import art
-print(art.logo)
-
-
-def deal_card():
+from art import logo
+def deal_card():  #This Function give me a random card
     cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
     card = random.choice(cards)
     return card
+
+def compare(my_score,computer_score): # returning the appropriate print satatement according to user and computer score
+    if my_score == computer_score:
+        return 'Draw!'
+    if my_score == 0:
+        return 'Win with a BlackJack 😂'
+    elif computer_score == 0:
+        return 'Lose ! opponent has Blackjack 😥'
+    elif my_score > 21:
+        return 'You went over, you lose 😮'
+    elif computer_score > 21:
+        return 'Win ! opponent has went over 😛'    
+    elif  my_score > computer_score:
+        return 'Win !🤩' 
+    else:
+        return 'lose !'
+
+def calculate_score(cards): #give cards of user or computer and calculate the score using sum() function
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0 
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+print(logo)
 user_cards = []
 machine_cards = []
-game_over = False
+is_game_over = False
+
 for _ in range(2):
     user_cards.append(deal_card())
     machine_cards.append(deal_card())
-print(f'\tyour cards: {user_cards}, current score: {sum(user_cards)}')
-print(f'\tcomputer\'s first card: {machine_cards[0]}')
-while not game_over:
-    continue_or_pass = input('Type \'y\' to get another card, type \'n\' to pass:\t').lower()
-    if continue_or_pass == 'y':
-        user_cards.append(deal_card())
-        machine_cards.append(deal_card())
-        print(f'\tyour cards: {user_cards}, current score: {sum(user_cards)}')
-        print(f'\tcomputer\'s first card: {machine_cards[0]}')
-    else:
-        game_over = True
 
-print(user_cards)
-print(machine_cards)
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    machine_score = calculate_score(machine_cards)
+    print(f"\tYour cards: {user_cards}, current score: {user_score}")
+    print(f"\tComputer's first card: {machine_cards[0]}")
+    if user_score == 0 or machine_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_deal = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        if user_deal == 'y':
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+while machine_score != 0 and machine_score < 17:
+    machine_cards.append(deal_card)
+    machine_acore = calculate_score(machine_cards)
+print(f"   Your final hand: {user_cards}, final score: {user_score}")
+print(f"   Computer's final hand: {machine_cards}, final score: {machine_score}")
+print(compare(user_score, machine_score))
+
