1a2225682a3d318fc298963be3015915084c889a
Bilal Ahmad <bilal2q2@gmail.com>
2021-01-14 18:14:39 +0500

    Day_14_final

--- hello.py
+++ hello.py
@@ -3,30 +3,30 @@ from Day_14_game_data import data
 from Day_14_Art import art
 from Day_14_Art import vs
 
+
+
+print(art)
 score = 0
 game_play = False
+
 while not game_play:
     first = random.randint(0,len(data)-1)
     second = random.randint(0,len(data)-1)
-    print('Compare A:', end=' ')
-    for f in data[first].values():
-        print(f, end=',')
-
+    first_player = data[first]['follower_count']
+    second_player = data[second]['follower_count']
+    print(f"{data[first]['follower_count']} Compare A: {data[first]['name']}, a {data[first]['description']}, from {data[first]['country']}")
     print(f'\n',vs)
+    print(f"{data[second]['follower_count']} Compare B: {data[second]['name']}, a {data[second]['description']}, from {data[second]['country']}")
 
-    print('Compare B:', end=' ')
-    for f in data[second].values():
-        print(f, end=',')
 
-    choice = input('\n\nWho has more folloers? Type \'A\' or \'B\': ').lower()
-    if choice == 'a':
-        if data[first]['follower_count'] > data[second]['follower_count'] :
-            score += 1
-    elif choice == 'b':
-        if data[first]['follower_count'] < data[second]['follower_count'] :
-            score += 1 
+    choice = input('\n\nWho has more folloers? Type \'A\' or \'B\': \n').lower()
+    if choice == 'a' and first_player >= second_player:
+        score += 1
+    elif choice == 'b' and data[first]['follower_count'] < data[second]['follower_count']:
+        score += 1 
     else:
         game_play = True
-    print(data[first]['follower_count'])
-    print(data[second]['follower_count'])
-    print(f'Your Final Score is {score}')
+        print('Wrong Choice !')
+    # print(data[first]['follower_count'])
+    # print(data[second]['follower_count'])
+    print(f'Your Final Score is {score} \n')
--- test.py
+++ test.py
@@ -1,9 +1,26 @@
-d={
-    'name': 'Real Madrid CF',
-    'follower_count': 86,
-    'description': 'Football club',
-    'country': 'Spain'
-}
+import random
+from Day_14_game_data import data
+from Day_14_Art import art
+from Day_14_Art import vs
 
-for value in d.values():
-    print(value, end=' ')
+score = 0
+game_play = False
+while not game_play:
+    first = random.randint(0,len(data)-1)
+    second = random.randint(0,len(data)-1)
+    print(f"{data[first]['follower_count']} Compare A: {data[first]['name']}, a {data[first]['description']}, from {data[first]['country']}")
+    print(f'\n',vs)
+    print(f"{data[second]['follower_count']} Compare B: {data[second]['name']}, a {data[second]['description']}, from {data[second]['country']}")
+
+
+    choice = input('\n\nWho has more folloers? Type \'A\' or \'B\': ').lower()
+    if choice == 'a' and data[first]['follower_count'] >= data[second]['follower_count']:
+        score += 1
+    elif choice == 'b' and data[first]['follower_count'] < data[second]['follower_count']:
+        score += 1 
+    else:
+        game_play = True
+        print('Wrong Choice !')
+    # print(data[first]['follower_count'])
+    # print(data[second]['follower_count'])
+    print(f'Your Final Score is {score}')
