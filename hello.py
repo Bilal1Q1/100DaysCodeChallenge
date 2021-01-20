2678eabcfeabe41a72b26dcc289a387f4a17564d
Bilal Ahmad <bilal2q2@gmail.com>
2021-01-20 18:33:58 +0500

    day_15_from Office

--- hello.py
+++ hello.py
@@ -1,25 +1,45 @@
 MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
+    'espresso':{
+        'ingredients' : {
+            'water' : 50,
+            'coffee' : 18
         },
-        "cost": 1.5,
+        'cost' : 1.5
     },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
+    'latte':{
+        'ingredients':{
+            'water' : 200,
+            'coffee' : 24,
+            'milk' : 150
         },
-        "cost": 2.5,
+        'cost' : 2.5
     },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
+    'cappuccino':{
+        'ingredients':{
+            'water' : 250,
+            'coffee' : 24,
+            'milk' : 100
         },
-        "cost": 3.0,
+        'cost' : 3.
     }
 }
+def is_resources_sufficient(order_ingredients):
+    
+profit = 0
+resources = {
+    'water': 300,
+    'milk' : 200,
+    'coffee' : 100
+}
+is_on = True
+choice = input('What would you like? (espresso/latte/cappuccino): ')
+if choice == 'off':
+    is_on = False
+elif choice == 'report':
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Cost: {profit}")
+else:
+    drink = MENU[choice]
+    is_resources_sufficient(drink['ingredients'])
--- test.py
+++ test.py
@@ -1,26 +1,8 @@
-import random
-from Day_14_game_data import data
-from Day_14_Art import art
-from Day_14_Art import vs
-
-score = 0
-game_play = False
-while not game_play:
-    first = random.randint(0,len(data)-1)
-    second = random.randint(0,len(data)-1)
-    print(f"{data[first]['follower_count']} Compare A: {data[first]['name']}, a {data[first]['description']}, from {data[first]['country']}")
-    print(f'\n',vs)
-    print(f"{data[second]['follower_count']} Compare B: {data[second]['name']}, a {data[second]['description']}, from {data[second]['country']}")
-
-
-    choice = input('\n\nWho has more folloers? Type \'A\' or \'B\': ').lower()
-    if choice == 'a' and data[first]['follower_count'] >= data[second]['follower_count']:
-        score += 1
-    elif choice == 'b' and data[first]['follower_count'] < data[second]['follower_count']:
-        score += 1 
-    else:
-        game_play = True
-        print('Wrong Choice !')
-    # print(data[first]['follower_count'])
-    # print(data[second]['follower_count'])
-    print(f'Your Final Score is {score}')
+resources = {
+    'water': 300,
+    'milk' : 200,
+    'coffee' : 100
+}
+order_ingredients = {'water' : 250,'coffee' : 24,'milk' : 100}
+resources['water'] - order_ingredients['water']
+print(resources)
