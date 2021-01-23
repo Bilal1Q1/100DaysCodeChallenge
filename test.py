498a5c9188c37d139851d46f8fddf1c6f12afd92
Bilal Ahmad <bilal2q2@gmail.com>
2021-01-23 15:25:20 +0500

    finally Completed Day 15 Coffee Machine

--- test.py
+++ test.py
@@ -3,6 +3,5 @@ resources = {
     'milk' : 200,
     'coffee' : 100
 }
-order_ingredients = {'water' : 250,'coffee' : 24,'milk' : 100}
-resources['water'] - order_ingredients['water']
-print(resources)
+for item in resources:
+    print(item)
